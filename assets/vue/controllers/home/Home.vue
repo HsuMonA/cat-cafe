628d7d9a11d9ceaae62b0fd854b6fdeeef41d3cf
HsuMonA <HsuMonA@users.noreply.github.com>
2023-05-29 16:38:48 +0200

    use scss, add vue and bootstrap

---
+++ assets/vue/controllers/home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Hello {{ name }}!</div>
+  <button type="button" class="btn btn-primary">Primary Button</button>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+});
+</script>
