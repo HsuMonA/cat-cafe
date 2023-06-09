b00ad7e7689f6ab4f2492bbcf973e595b49dd426
HsuMonA <HsuMonA@users.noreply.github.com>
2023-06-09 16:30:32 +0200

    add user fields

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -9,16 +9,22 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('firstName', TextType::class, ['mapped' => false])
+            ->add('lastName', TextType::class, ['mapped' => false])
+            ->add('email', EmailType::class, ['mapped' => false])
+            ->add('telephoneNumber', TelType::class, ['mapped' => false])
             ->add('date')
             ->add('numberOfPersons')
-            ->add('user')
             ->add('service', EntityType::class, [
                 'class' => Service::class,
                 'choice_label' => 'type',
