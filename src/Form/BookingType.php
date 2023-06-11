add404b874e0be490eab79c97fca82b1ee44e7c2
HsuMonA <HsuMonA@users.noreply.github.com>
2023-06-11 12:59:42 +0200

    add symfony validations

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -9,29 +9,61 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class, ['mapped' => false])
-            ->add('lastName', TextType::class, ['mapped' => false])
-            ->add('email', EmailType::class, ['mapped' => false])
-            ->add('telephoneNumber', TelType::class, ['mapped' => false])
-            ->add('date')
-            ->add('numberOfPersons')
+            ->add('firstName', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 100])
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 100])
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new Assert\Email(),
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 200])
+                ]
+            ])
+            ->add('telephoneNumber', TelType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(['max' => 50])
+                ]
+            ])
+            ->add('date', DateTimeType::class, ['constraints' => [new Assert\GreaterThan('today')]])
+            ->add('numberOfPersons', IntegerType::class, ['constraints' => [new Assert\Range(['min' => 1, 'max' => 5])]])
             ->add('service', EntityType::class, [
                 'class' => Service::class,
                 'choice_label' => 'type',
-
                 // used to render a select box, check boxes or radios
                 'multiple' => false,
                 'expanded' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                ]
             ])
             ->add('submit', SubmitType::class, ['label' => 'Submit'])
             ->add('cancel', ButtonType::class, ['label' => 'Cancel'])
